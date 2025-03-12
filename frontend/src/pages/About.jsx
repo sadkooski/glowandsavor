755dc81882963b3612328ccbee09c95da95502a3
Oskar Sadkowski <mrproskar@gmail.com>
2025-03-12 22:37:55 +0100

    About me page structure and styling

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,25 +3,42 @@ import React from "react";
 const About = () => {
     return (
 <section className="w-full bg-white">
-    <div className="flex flex-row w-full h-fit relative px-[12vw]">
+    <div className="flex flex-col w-full h-[55vw] px-[7vw] mb-[5vw] relative">
             <img src="/assets/img-4.jpg"
                 alt="Kontakt"
-                className="w-[40vw] h-fit object-contain object-right-top z-1 "/>
-            <div className="bg-gray-100 text-left flex flex-col gap-[2vw] py-[4vw] px-[10vw] w-[45vw] h-fit absolute top-[10%] left-[45%] z-0 ">
-                <h3 className="text-[2vw]">Lorem ipsum dolor sir, a elit. Alias, excepturi totam aliquam provident doloremque explicabo s
+                className="w-[38vw] h-full object-cover object-right-top z-1"/>
+            <div className="bg-gray-100 text-left flex flex-col gap-[2vw] pt-[10vw] pl-[12vw] pr-[8vw] pb-[10vw] w-[54vw] h-[53vw] absolute top-[7vw] left-[38vw] z-0">
+                <h3 className="text-[1.6vw]">Lorem ipsum dolor sir, a elit. Alias, excepturi totam aliquam provident doloremque explicabo s
                 </h3>
-                <p className="text-[1.7vw]">Lorem ipsum dolor sit ia, doloribus eviumremque tempore quae dolores? Unde soluta dolorum accusamus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dicta dolorum temporibus quos omnis nobis repudiandae cumque vel non, corporis, veniam ratione. Nam autem voluptatem nihil accusantium illum amet commodi.</p>
+                <p className="text-[1.4vw]">Lorem ipsum dolor sit ia, dolodfgsdfgs dfgsdge egergergerg dfgdfsgdsf gedged fgdfgdfgdfg dgdfgdfgd fgdfg dfg dfgdfg dfg gdfgdfgdf dgd gdf dgdfgdfgdfg dgrgergdfgdsfg fgsdfggreger gdfgdfg ergedfgdfribus eviumremque tempore quae dolores? Unde soluta dolorum accusamus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dicta dolorum temporibus quos omnis nobis repudiandae cumque vel non, corporis, veniam ratione. Nam autem voluptatem nihil accusantium illum amet commodi.</p>
             </div>
     </div>
 
-    <div className="flex flex-row w-full h-[60vh] mt-[12vw] pr-[8vw] text-left">
-            <p className="text-[1.7vw] pt-[2vw] px-[10vw]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos praesentium, alias neque necessitatibus impedit est, vero itaque molestiae consequatur, esse ut. Odio eligendi ea numquam praesentium dolores, quis sapiente voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quam dolorem doloribus voluptas, tenetur illo quia sequi magnam suscipit vitae accusantium eveniet similique quibusdam eius amet delectus tempora repudiandae consectetur!Lorem</p>
-        <img src="/assets/img-5.jpg"
+    <div className="flex flex-row w-full h-fit text-left">
+        <div className="flex flex-col text-[1.4vw] px-[3vw] pt-[6vw] gap-[2vw]">
+            <span className="flex justify-center">Londa dignissimolendus minus!</span>
+            <p className="">Lorem ipsum,  provident, , adipisicing elit. Vel at, offg elit. illcipit vit eveniet simuibusdam eius amet delectus tempora repudiandae consectetur!Lorem</p>
+            <p className="">Lorem ipsum,  provident, , enda oquam praesentium dolores, quis sapien sit amet consectetur adipisicing elit. Mollitia quam dolorem doloribus voluptas, tenetur illo quia sequi magnam suscipit vitae accusantium eveniet similique quibusdam eius amet delectus tempora repudiandae consectetur!Lorem</p>
+            <p className="">Lorem ipsum, dolor sit amet ctionem libero neque dignipis nobis suscipit perspiaudantium et in ipsa eos!Lorem ipsum dolor sit, amet consectecing elit. Qus s itaque molestiae consequatur, esse ut. Odio eligendi ea numquam praesentium dolores, quis sapiente voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quam dolorem doloribus voluptas, tenetur illo quia sequi magnam suscipit vitae accusantium eveniet similique quibusdam eius amet delectus tempora repudiandae consectetur!Lorem</p>
+        </div>
+        <img src="/assets/img-8.jpg"
                 alt="Kontakt"
-                className="w-[40vw] object-cover" />
+                className="w-[58vw] object-cover" />
+    </div>
+
+    <div className="flex flex-row w-full mt-[5vw] pl-[2vw] gap-[1.5vw]">
+        <div className="flex flex-col w-[65vw] gap-[2vw]">
+            <img className="" src="/assets/img-7.jpg" alt="" />
+            <p className="text-[1.4vw] px-[1vw] text-left">Lorem ipsum, dolor sit amet consectetur adipisicin ipt vitae tenetur. Et, molestiae minus quasi delectus quae vitae?</p>
+        </div>
+        <div className="flex flex-col w-[35vw] gap-[2vw]">
+            <img className="h-[24vw]" src="/assets/img-5.jpg" alt="" />
+            <p className="text-[1.4vw] px-[2vw]"> Lorem ipsum dolor sit amet consectetur adipisicings quas doloremque fuga.</p>
+            <img className="h-[24vw]" src="/assets/img-9.jpg" alt="" />
+        </div>
     </div>
 </section>
     )
 }
 
-export default About;
+export default About
