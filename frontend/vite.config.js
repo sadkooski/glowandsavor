b59a78b20081fb3aefa52d3bd19c4f6325ef4ce2
Oskar Sadkowski <mrproskar@gmail.com>
2025-03-14 00:22:38 +0100

    4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  base: '/glow-savor/', // ZMIEŃ na nazwę swojego repozytorium
+  build: {
+    outDir: 'dist', // Folder, gdzie będzie generowany build
+  },
+});
