f7777c8e9df1a1f23850ad50329b7b3efb2f1945
Oskar Sadkowski <mrproskar@gmail.com>
2025-04-21 23:32:33 +0200

    1

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -98,7 +98,7 @@ const ContactForm = () => {
   
     if (validateForm()) {
       try {
-        const response = await fetch("http://localhost:5000/api/send-email", {
+        const response = await fetch("https://glowandsavor-backend.onrender.com/api/send-email", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
