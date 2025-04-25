eb8679f18c297dde288f4334bcd862cbfdf652b5
Oskar Sadkowski <mrproskar@gmail.com>
2025-04-25 14:16:33 +0200

    ebook page edited

--- frontend/src/pages/Ebook.jsx
+++ frontend/src/pages/Ebook.jsx
@@ -1,69 +1,39 @@
 import React from "react";
+import EbookPurchaseSection from "../components/EbookPurchaseSection";
 
 const EbookPage = () => {
     return (
-        <div className=" bg-gray-100 flex flex-col items-center justify-center py-[4vw]">
-            <div className="w-[80vw] mx-auto bg-white shadow-lg rounded-lg p-[2vw]">
+        <div className="bg-gray-100 flex flex-col items-center justify-center py-[4vw]">
+            <div className="md:w-[80vw] mx-auto bg-white md:shadow-lg md:rounded-lg p-[4vw] mb-[4vw]">
                 {/* Nagłówek */}
-                <h1 className="text-[3.5vw] font-bold text-center text-gray-800 mb-[2vw]">
+                <h2 className="md:text-[3.5vw] text-2xl font-bold text-center text-gray-800 mb-[4vw]">
                     Koktajle białkowo-kolagenowe
-                </h1>
+                </h2>
 
-                {/* Zdjęcia */}
-                <div className="grid grid-cols-2 md:grid-cols-2 gap-[2vw] mb-6">
-                    <div>
-                        <img
-                            src="/assets/ebook-1.jpg"
-                            alt="Ebook - Kolagen"
-                            loading="lazy"
-                            className="w-full h-[30vw] object-cover rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="/assets/ebook-2.jpg"
-                            alt="Koktajl"
-                            loading="lazy"
-                            className="w-full h-[30vw] object-cover rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="/assets/ebook-3.jpg"
-                            alt="Ebook - Kolagen"
-                            loading="lazy"
-                            className="w-full h-[30vw] object-cover rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="/assets/ebook-4.jpg"
-                            alt="Koktajl"
-                            loading="lazy"
-                            className="w-full h-[30vw] object-cover rounded-lg"
-                        />
-                    </div>
+                {/* Zdjęcia (tylko 2 pierwsze) */}
+                <div className="grid md:grid-cols-2 gap-[2vw] pb-[4vw]">
+                    {["ebook-1", "ebook-2"].map((img, idx) => (
+                        <div key={idx}>
+                            <img
+                                src={`/assets/${img}.jpg`}
+                                alt={`Ebook - ${idx + 1}`}
+                                loading="lazy"
+                                className="w-full md:h-[30vw] h-[70vw] object-cover rounded-lg"
+                            />
+                        </div>
+                    ))}
                 </div>
 
                 {/* Opis */}
-                <p className="text-[2vw] text-gray-700 text-center my-[3vw] mx-[3vw]">
+                <p className="md:text-[2vw] text-gray-700 text-center my-[3vw] md:my-0 mx-[3vw]">
                     Dowiedz się, jak w prosty sposób przygotować pyszne koktajle z kolagenem, które
                     poprawią kondycję Twojej skóry i zdrowia. Mój ebook to zbiór przepisów na zdrowe
                     napoje, które możesz łatwo wprowadzić do swojej codziennej diety.
                 </p>
-
-                {/* Przycisk Kup Teraz */}
-                <div className="flex justify-center">
-                    <a
-                        href="https://allegro.pl/aukcja/your-ebook-link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="bg-blue-600 text-white px-[3vw] py-[1vw] rounded-md text-[2vw] hover:bg-blue-700 transition"
-                    >
-                        Kup Teraz
-                    </a>
-                </div>
             </div>
+
+            {/* Komponent z zakupem e-booka */}
+            <EbookPurchaseSection />
         </div>
     );
 };
